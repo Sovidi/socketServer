7d4150288d9e2d56213d5da277383d8bf03ae23d
Sovidi <lim132445@gmail.com>
2025-06-19 13:39:28 +0900

    주소값 변경

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ app.use(bodyParser.json());
 const queryExecute = require("./src/connection.js");
 // const data = require("./src/dataModule.js");
 
-app.get("/select", async function (req, res) {
+app.get("/lcm_select", async function (req, res) {
   console.log("select 요청");
   let getData = await queryExecute("select * from todotxt");
   res.send(getData);
 });
 
-app.post("/insert", async function (req, res) {
+app.post("/lcm_insert", async function (req, res) {
   const bodyData = req.body;
   await queryExecute("insert into todotxt (sKey, txt) values (?, ?)", [bodyData.key, bodyData.txt]);
   let getData = await queryExecute("select * from todotxt");
@@ -27,7 +27,7 @@ app.post("/insert", async function (req, res) {
   res.send(getData);
 });
 
-app.delete(`/delete`, async function (req, res) {
+app.delete(`/lcm_delete`, async function (req, res) {
   const { key } = req.query;
   await queryExecute("delete from todotxt where sKey=?", [key]);
 
@@ -35,7 +35,7 @@ app.delete(`/delete`, async function (req, res) {
   res.send(getData);
 });
 
-app.put(`/update`, async function (req, res) {
+app.put(`/lcm_update`, async function (req, res) {
   const bodyData = req.body;
   await queryExecute("update todotxt set txt=? where sKey=?", [bodyData.txt, bodyData.key]);
 
@@ -44,7 +44,7 @@ app.put(`/update`, async function (req, res) {
 });
 
 // 댓글삭제 http 연결 형식
-app.delete(`/chatDelete`, async function (req, res) {
+app.delete(`/lcm_chat_delete`, async function (req, res) {
   const { sKey } = req.query;
   await queryExecute("delete from chats where sKey=?", [sKey]);
   let getData = await queryExecute("select * from chats");
